b01bd841b5ea179fda09bed54f24480d66bc4a29
Kawaii Programming <34284353+kawaaii@users.noreply.github.com>
2018-09-23 17:41:30 +0200

    Added Webhook for outputs

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const config = require('./data/config.json');
 const roles = require('./data/roles.json');
 const {token, wh_id, wh_token} = require('./data/token.json'); 
 const client = new Discord.Client({autoReconnect:true});
+const webhook = new Discord.WebhookClient(wh_id, wh_token);
 
 client.on('ready', () => {
     // Messages the bot will sent in the console, when the bot is ready and connected.
@@ -15,7 +16,7 @@ client.on('guildMemberAdd', member => {
     // The bot will automaticly assign the role to the new member if enough permissions.
     if (roles.joinRole) {
         member.addRole(roles.joinRole)
-        .catch((reason) => { console.log(reason) });
+        .catch((reason) => { webhook.send(reason) });
     };
 });
 
@@ -25,14 +26,14 @@ client.on('presenceUpdate', (oldMember, newMember) => {
         if (roles.fortniteRole && newMember.presence.game.name == "Fortnite") {
             if (!newMember.roles.has(roles.fortniteRole)) {
                 newMember.addRole(roles.fortniteRole)
-                .then(console.log(`Fortnite role has been set to ${newMember.user.username}`));
+                .then(webhook.send(`Fortnite role has been set to ${newMember.user}`));
             }
         }
 
         if (roles.minecraftRole && newMember.presence.game.name == "Minecraft") {
             if (!newMember.roles.has(roles.minecraftRole)) {
                 newMember.addRole(roles.minecraftRole)
-                .then(console.log(`Minecraft role has been set to ${newMember.user.username}`));
+                .then(webhook.send(`Minecraft role has been set to ${newMember.user}`));
             }
         }
     }
