a21e0f858744ec807f9a327d8155d039fb39a197
Kawaii Programming <34284353+kawaaii@users.noreply.github.com>
2018-11-09 02:15:50 +0100

    Changed some of the code!

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const Discord = require('discord.js');
+const fs = require('fs');
 const config = require('./data/config.json');
 const roles = require('./data/roles.json');
 const {token, wh_id, wh_token} = require('./data/token.json'); 
 const client = new Discord.Client({autoReconnect:true});
 const webhook = new Discord.WebhookClient(wh_id, wh_token);
 
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./cmds').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./cmds/${file}`);
+    client.commands.set(command.name, command);
+}
+
 client.on('ready', () => {
-    // Messages the bot will sent in the console, when the bot is ready and connected.
     console.log(`I'm ready and running!`);
     console.log(`${client.users.size} members found.`)
-    console.log(roles.PresenceRoles);
+    console.log(roles.SelfAssignableRoles);
 });
 
 client.on('guildMemberAdd', member => {
-    // If a role in setup in the config file.
-    // The bot will automaticly assign the role to the new member if enough permissions.
     if (roles.joinRole) {
         member.addRole(roles.joinRole)
         .then(() => webhook.send(`**New Member has Joined:** ${member}`))
@@ -22,31 +28,23 @@ client.on('guildMemberAdd', member => {
     };
 });
 
-client.on('presenceUpdate', (oldMember, newMember) => {
-
-    if (newMember.presence.game) {
-        if (roles.PresenceRoles.find(item=>item.gameName == newMember.presence.game.name)) {
-            const gameRole = roles.PresenceRoles.find(item=>item.gameName == newMember.presence.game.name);
-            if (!newMember.roles.has(gameRole.roleID)) {
-                newMember.addRole(gameRole.roleID)
-                .then((member) => {
-                    try {
-                        member.send(`You have been assigned the "${gameRole.gameName}" role automatically.\nBecause I detected that you were playing **${gameRole.gameName}**.`);
-                    } catch (e) { };
-                    webhook.send(`**${gameRole.gameName}** role has been given to ${member.user}`);
-                })
-                .catch((error) => {console.log(error)})
-            }
-        }
-    }
-
-});
-
 client.on('message', message => {
-    if (!config.owners.includes(message.author.id)) {
-        // If message author is not included in the config file, bot will ignore the commands.
-        return;
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+    if (!config.owners.includes(message.author.id)) return;
+    
+    const args = message.content.slice(config.prefix.length).split(/ +/);
+	const commandName = args.shift().toLowerCase();
+
+    const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    if (!command) return;
+
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
     };
+
 });
 
 client.login(token);
